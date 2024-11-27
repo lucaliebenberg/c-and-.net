12459fd00e590e247eff64999388bd86caa71ec1
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-11-27 22:27:41 +0200

    chore: WIP commit for working with LINQ

--- FirstDotnetApp/obj/Debug/net8.0/FirstDotnetApp.AssemblyInfo.cs
+++ FirstDotnetApp/obj/Debug/net8.0/FirstDotnetApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstDotnetApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+793cd5a8bca2e09aad8b2715539af170b5a55873")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+200f815c82c9909bf781a84521c72b1658bbf837")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstDotnetApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstDotnetApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
